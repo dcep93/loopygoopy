b4d217a1fdd8347e8457cc46278cff85738363ff
dcep93 <dan@quizlet.com>
2020-04-02 23:04:54 -0700

    form

--- extension/popup.js
+++ extension/popup.js
@@ -2,17 +2,39 @@ var tabId;
 chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
 	tabId = tabs[0].id;
 });
-var ids = ["bpm", "bpl", "bpr"];
-var elements = {};
-for (var i = 0; i < ids.length; i++) {
-	var id = ids[i];
-	elements[id] = document.getElementById(id);
+
+var submitInput = document.createElement("input");
+submitInput.type = "submit";
+submitInput.style = "display: none";
+var inputsRaw = document.getElementsByTagName("input");
+var inputs = Array.from(inputsRaw);
+for (var i = 0; i < inputs.length; i++) {
+	var element = inputs[i];
+	element.parentElement.insertBefore(
+		submitInput.cloneNode(),
+		element.nextSibling
+	);
 }
+
+var form = document.getElementById("submit");
+
 function submit() {
+	var formData = new FormData(form);
 	var message = {};
-	for (let key in elements) {
-		message[key] = elements[key].value;
-	}
-	chrome.tabs.sendMessage(tabId, message);
+	formData.forEach((value, key) => {
+		message[key] = value;
+	});
+	sendMessage("submit", message);
+	return false;
 }
-document.getElementById("submit").onsubmit = submit;
+
+var buttonNames = ["start", "stop", "next", "previous"];
+buttonNames.forEach(name => {
+	document.getElementById(name).onclick = () => sendMessage(name);
+});
+
+function sendMessage(type, message) {
+	chrome.tabs.sendMessage(tabId, { type, message });
+}
+
+form.onsubmit = submit;
