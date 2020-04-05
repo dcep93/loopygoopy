e85672c7360e4e065b64ca6c152a0d6e091d91f0
dcep93 <dan@quizlet.com>
2020-04-04 18:32:09 -0700

    save default

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,5 @@
 var tabId;
-chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 	tabId = tabs[0].id;
 	chrome.tabs.sendMessage(tabId, { type: "load" });
 });
@@ -11,6 +11,7 @@ var inputsRaw = document.getElementsByTagName("input");
 var inputs = Array.from(inputsRaw);
 for (var i = 0; i < inputs.length; i++) {
 	var element = inputs[i];
+	element.onchange = saveDefault;
 	element.parentElement.insertBefore(
 		submitInput.cloneNode(),
 		element.nextSibling
@@ -20,18 +21,13 @@ for (var i = 0; i < inputs.length; i++) {
 var form = document.getElementById("form");
 
 function sendMessage(type) {
-	var formData = new FormData(form);
-	var message = {};
-	formData.forEach((value, key) => {
-		message[key] = value;
-	});
-	chrome.storage.sync.set({ [state.id]: message });
+	var message = saveForm(state.id);
 	chrome.tabs.sendMessage(tabId, { type, message });
 	return false;
 }
 
 var buttonNames = ["start", "stop", "next", "previous"];
-buttonNames.forEach(type => {
+buttonNames.forEach((type) => {
 	document.getElementById(type).onclick = () => sendMessage(type);
 });
 
@@ -58,15 +54,39 @@ document.getElementsByTagName("html")[0].style.height = form.offsetHeight;
 
 var state = {};
 var stInput = document.getElementById("st");
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	if (message.currentTime) stInput.value = message.currentTime;
 	if (state.id != message.id) {
-		var key = message.id;
-		chrome.storage.sync.get([key], function(result) {
-			var object = result[key];
-			for (var name in object) form[name].value = object[name];
-		});
+		setForm(message.id);
 	}
 	state.id = message.id;
 	sendResponse(true);
 });
+
+function saveDefault() {
+	saveForm("default");
+}
+
+function saveForm(id) {
+	var formData = new FormData(form);
+	var message = {};
+	formData.forEach((value, key) => {
+		message[key] = value;
+	});
+	chrome.storage.sync.set({ [id]: message });
+	console.log("save", id, message);
+	return message;
+}
+
+function setForm(id) {
+	console.log("set", id);
+	chrome.storage.sync.get([id], function (result) {
+		var object = result[id];
+		console.log("set", id, object);
+		for (var name in object) form[name].value = object[name];
+	});
+}
+
+if (!state.id) {
+	setForm("default");
+}
