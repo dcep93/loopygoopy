a4265840fcdf81b0b7a797e2eede899175df616b
Daniel Cepeda <dcep93@gmail.com>
2025-04-17 02:24:52 -0400

    x

--- extension/app/src/app/LoopyGoopy/Notes.tsx
+++ extension/app/src/app/LoopyGoopy/Notes.tsx
@@ -1,22 +1,25 @@
-import { Field, state, updateInput } from "./brain";
+import { Field, refs, state, updateInput } from "./brain";
 import { storageKey } from "./storage";
 
 // @ts-ignore
-import lodash from "lodash"; // todo
+import { debounce } from "lodash"; // todo
 
 export default function Notes() {
+  const debouncer = debounce(
+    () => updateInput(Field.notes, refs[Field.notes].current.value, false),
+    100,
+    { leading: true, trailing: true }
+  );
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <div>notes: {storageKey}</div>
       <textarea
+        ref={
+          refs[Field.notes] as unknown as React.RefObject<HTMLTextAreaElement>
+        }
         defaultValue={state[Field.notes]}
         style={{ flexGrow: 1 }}
-        onChange={(e) =>
-          lodash.debounce(
-            () => updateInput(Field.notes, e.target.value, false),
-            100
-          )()
-        }
+        onChange={debouncer}
       ></textarea>
     </div>
   );
