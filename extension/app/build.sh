850487e88cfdafa4fc8b366347de5db437369a69
Daniel Cepeda <dcep93@gmail.com>
2026-04-11 00:03:11 -0400

    x

--- extension/app/build.sh
+++ extension/app/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd src/app && tsc
+set -euo pipefail
+
+(cd src/app && tsc)
 PUBLIC_URL=/app/build/ yarn build
+cd ../../
 zip -r extension.zip extension -x "*/node_modules/*" -x "*/app/*/manifest.json"
